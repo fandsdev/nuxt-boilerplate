483e7c63be17300df201a2c2496b1995a811f01b
Mikhail Burmistrov <misha.burmistrov@gmail.com>
2022-03-16 23:31:41 +0300

    Setup styles (#2)
    * Add destyle css & custom media variables
    
    * Add styles to index to showcase css modules & custom media variables
    
    * Allow multi word component names for pages & layouts

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,25 @@
 <template>
-  <Tutorial />
+  <div :class="$style.root">
+    <div :class="$style.greeting">Hello there 👋</div>
+  </div>
 </template>
 
-<script>
-export default {
-  name: 'IndexPage',
+<style module>
+.root {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  min-height: 100vh;
 }
-</script>
+
+.greeting {
+  font-size: 120px;
+}
+
+@media (--small-vp) {
+  .greeting {
+    font-size: 40px;
+  }
+}
+</style>
