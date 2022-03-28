b3a1bf41d821a315afff134c7a0087f479419d3e
mburmistrov <misha.burmistrov@gmail.com>
2022-03-29 01:45:15 +0300

    Add default layout

---
+++ layouts/default.vue
@@ -0,0 +1,30 @@
+<template>
+  <div :class="$style.root">
+    <BaseIconSprite />
+    <TheNavbar />
+    <div :class="$style.main">
+      <Nuxt />
+    </div>
+    <TheFooter />
+  </div>
+</template>
+
+<script>
+import BaseIconSprite from '~/components/_base/icon/_base-icon-sprite.vue';
+import TheNavbar from '~/components/the-navbar.vue';
+
+export default {
+  components: {
+    BaseIconSprite,
+    TheNavbar,
+  },
+};
+</script>
+
+<style lang="postcss" module>
+.main {
+  max-width: 1300px;
+  min-height: calc(100vh - var(--navbar-height));
+  margin: auto;
+}
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['destyle.css/destyle.min.css'],
+  css: ['destyle.css/destyle.min.css', '~/assets/variables.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
