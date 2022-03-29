08a0eb0e80aab37fdd38c871d5f06cb144f76405
mburmistrov <misha.burmistrov@gmail.com>
2022-03-30 02:52:42 +0300

    Disable auto import components

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,9 +22,6 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: ['~/plugins/i18n'],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
-
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/eslint
